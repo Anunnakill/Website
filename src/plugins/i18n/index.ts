4d74a993dc92a23d94eb8e9fdbeb513eeec5f844
JICHENG <psycho@JICHENGdeMBP.lan>
2023-04-12 14:25:45 +0800

    update

--- src/plugins/i18n/index.ts
+++ src/plugins/i18n/index.ts
@@ -1,9 +1,7 @@
 import en from "./lan/en.json";
 import { createI18n } from "vue-i18n";
-import { readLanguage } from "@/utils/language";
 
 export default createI18n({
-  locale: readLanguage(),
   legacy: false,
   messages: {
     en,
