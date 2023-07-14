3f113168e06e17ed065d33fa16b6c1402a344c1e
Anunnakill <jicheng.eleven@gmail.com>
2023-07-14 22:59:34 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { createApp } from "vue";
-
-import App from "./App.vue";
-import router from "./router";
-import i18n from "./plugins/i18n";
-import naive from "./plugins/naive";
 import { createPinia } from "pinia";
+
+import App from "@/App.vue";
+import router from "@/router";
+import i18n from "@/plugins/i18n";
+import naive from "@/plugins/naive";
 import VueAnimXyz from "@animxyz/vue3";
-import Directive from "./plugins/directive";
-import GoogleRecaptcha from "./plugins/recaptcha";
+import Directive from "@/plugins/directive";
+import GoogleRecaptcha from "@/plugins/recaptcha";
 
 import "atropos/scss";
 import "normalize.css";
 import "@animxyz/core";
-import "./assets/font/index.scss";
+import "@/assets/font/index.scss";
 import "cropperjs/dist/cropper.min.css";
-import "./assets/style/global/reset.scss";
-import "./assets/style/global/animate.scss";
+import "@/assets/style/global/reset.scss";
+import "@/assets/style/global/animate.scss";
 
 createApp(App)
   .use(i18n)
