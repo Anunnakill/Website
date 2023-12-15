4094fe906c8e3dfec7b9e554784e37722c77dff0
Anunnakill <jicheng.eleven@gmail.com>
2023-12-15 17:36:54 +0800

    update

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,1 @@
-import soon from "@/assets/images/events/soon.svg";
-import end from "@/assets/images/events/end.svg";
-import live from "@/assets/images/events/live.svg";
-
-const checkTimeStatusToImage = (startTime: string, endTime: string) => {
-  const startTimestamp = new Date(startTime).getTime();
-  const endTimestamp = new Date(endTime).getTime();
-  const currentTimestamp = new Date().getTime();
-
-  if (currentTimestamp < startTimestamp) return soon;
-  if (currentTimestamp > endTimestamp) return end;
-  return live;
-};
-
-export { checkTimeStatusToImage };
+export {};
